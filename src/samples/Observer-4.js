b62db5f92513acf3ffce8b70a1dde3b193f5ff2b
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2019-04-12 22:38:59 +0300

    new features with HTML | CSS | JS

--- src/samples/Observer-4.js
+++ src/samples/Observer-4.js
@@ -1,3 +1,11 @@
 const subscriber = document.body.appendChild (
     document.createElement ( "section" )
 )
+
+subscriber.style = `
+    border: inset 1px;
+    padding: 10px 20px;
+    font-family: Arial;
+    color: darkblue;
+    margin: 20px;
+`
