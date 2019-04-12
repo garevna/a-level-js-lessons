351260c7e966360c624d1579389824f0a655f0f8
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2019-04-12 11:53:16 +0300

    new features with HTML | CSS | JS

--- src/start.js
+++ src/start.js
@@ -1,2 +1,4 @@
 const topic = location.hash.slice(1)
-alert(topic)
+location.href = topic.indexOf ( ".md" ) >= 0 ?
+    `https://github.com/garevna/a-level-js-lessons/blob/master/md/${topic}` :
+    `https://garevna.github.io/a-level-js-lessons/html/${topic}`
