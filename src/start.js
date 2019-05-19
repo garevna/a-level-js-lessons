174dd4fe18a192f190b19db8622866db18fbe877
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2019-05-19 21:05:45 +0300

    v.2.1.0

--- src/start.js
+++ src/start.js
@@ -1,11 +1,9 @@
-const topic = location.hash.slice(1)
-console.log ( topic )
-const pageElement = document.getElementsByTagName ( "page-element" )[0]
-
-pageElement.setAttribute (
-  "src",
-  `src/lessons/${topic}.md`
-)
-// location.href = topic.indexOf ( ".md" ) >= 0 ?
-//     `https://github.com/garevna/a-level-js-lessons/blob/master/md/${topic}` :
-//     `https://garevna.github.io/a-level-js-lessons/html/${topic}`
+window.onhashchange = function ( event ) {
+  const topic = location.hash.slice(1)
+  console.log ( topic )
+  const pageElement = document.getElementsByTagName ( "page-element" )[0]
+  pageElement.setAttribute (
+    "src",
+    `src/lessons/${topic}.md`
+  )
+}
