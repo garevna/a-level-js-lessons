f551c2ae9487edff9f05a7173384cac0d6272c09
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2019-05-19 22:32:59 +0300

    v.2.1.1

--- src/start.js
+++ src/start.js
@@ -1,8 +1,8 @@
-window.onhashchange = function ( event ) {
-  document.getElementsByTagName ( "page-element" )[0]
-      .setAttribute (
-          "src",
-          `src/lessons/${location.hash.slice(1)}.md`
-      )
-      console.log ( document.getElementsByTagName ( "page-element" )[0] )
-}
+// window.onhashchange = function ( event ) {
+//   document.getElementsByTagName ( "page-element" )[0]
+//       .setAttribute (
+//           "src",
+//           `src/lessons/${location.hash.slice(1)}.md`
+//       )
+//       console.log ( document.getElementsByTagName ( "page-element" )[0] )
+// }
